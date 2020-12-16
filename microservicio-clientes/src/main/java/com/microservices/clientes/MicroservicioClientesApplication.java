d9947c2c56a6ddd41fe29a72efab78a29d8d303b
Miguel Armas <miguel.armas@jycleadingcorp.com>
2020-12-16 13:33:54 -0500

    master: se agrego la configuracion de service products y compras para eureka

--- microservicio-clientes/src/main/java/com/microservices/clientes/MicroservicioClientesApplication.java
+++ microservicio-clientes/src/main/java/com/microservices/clientes/MicroservicioClientesApplication.java
@@ -2,8 +2,10 @@ package com.microservices.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicioClientesApplication {
 
 	public static void main(String[] args) {
